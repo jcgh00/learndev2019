e650686b5cf237a80c089630c4d54135008c9ad3
Johnston <jcgh00@okdaily.com>
2019-03-21 12:29:42 -0700

    putResult()

--- calc1.js
+++ calc1.js
@@ -1,8 +1,18 @@
 //3-21-2019 jc
 //javascript code go es h ere
-function add1(){
-    return     0;
+function add1(val){
+    return val+10;
+}
+function complexCalc(val,rate){
+    return val+30*rate;
 }
 function alert1(){
+    console.log(1);
    alert (1);
+}
+function putResult(){
+    var tmp=parseFloat(fm1.amount.value);
+    var rt=5.5;
+    var result=complexCalc(tmp, rt);
+    fm1.result.value= result;
 }
